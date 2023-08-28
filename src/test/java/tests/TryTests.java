059a0a331e63ed5c444276f48eeec80b2d5f82a7
agjane <jn.moon@yandex.ru>
2023-08-28 17:39:28 +0300

    Добавление позитивного и негативного апи-теста

--- src/test/java/tests/TryTests.java
+++ src/test/java/tests/TryTests.java
@@ -1,10 +1,14 @@
 package tests;
 
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.get;
+import static io.restassured.http.ContentType.JSON;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static  org.hamcrest.Matchers.is;
 
 public class TryTests {
 
@@ -26,5 +30,84 @@ public class TryTests {
 
         assertEquals(expectedTotal, actualTotal);
     }
-}
 
+    @Test
+
+    void check401WDHubStatus() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(401);
+    }
+    @Test
+    void checkWDHubStatusOpenAuth() {
+        given()
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://user1:1234@selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+
+    @Test
+    void checkWDHubStatusHiddenAuth() {
+        given()
+                .auth().basic("user1","1234")
+                .log().uri()
+                .log().body()
+                .when()
+                .get("https://selenoid.autotests.cloud/wd/hub/status")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("value.ready", is(true));
+    }
+    @Test
+
+    void postReq() {
+        String body = "{\"email\": \"eve.holt@reqres.in\"," +
+        "\"password\": \"cityslicha\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .contentType(JSON)
+                .body(body)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+
+    void postReqNegative400() {
+        String body = "{\"email\": \"eve.holt@reqres.in\"," +
+                "\"password\": \"cityslicha\" }";
+
+        given()
+                .log().uri()
+                .log().body()
+                .body("123")
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is ("Missing email or username"));
+    }
+}
