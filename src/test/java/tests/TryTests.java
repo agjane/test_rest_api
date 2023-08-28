04bbcc2f08e8db5a8d97d0720a27ce2647f122bb
agjane <jn.moon@yandex.ru>
2023-08-28 09:54:43 +0300

    Вывод в консоль

--- src/test/java/tests/TryTests.java
+++ src/test/java/tests/TryTests.java
@@ -3,17 +3,21 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
 public class TryTests {
 
     @Test
     void checkTotal() {
-        get("https://selenoid.autotests.cloud/status")
+        given()
+                .log().all()
+                .when()
+                .get("https://selenoid.autotests.cloud/status")
                 .then()
+                .log().all()
                 .statusCode(200)
                 .body("total", is(20));
-
     }
 }
 
